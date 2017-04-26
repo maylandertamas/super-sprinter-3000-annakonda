25251155bab62929df5c83cfbfde051973caf45e
maylandertamas <maylandertamas@gmail.com>
2017-04-26 19:36:03 +0200

    Almost done

--- core.py
+++ core.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
-import csv
+
+
 app = Flask(__name__)
 
 
@@ -9,11 +10,16 @@ def home_list(data_list=[], methods=['GET', 'POST']):
     data_table = get_table_from_file()
     return render_template("list.html", data_list=data_table)
 
-"""
 @app.route('/story/<story_id>', methods=['GET', 'POST'])
-def story_edit(story_id=None):
-    return render_template("form.html",  story_id=story_id)
-"""
+def story_edit(story_id=None, form_data=[]):
+    table = get_table_from_file()
+    ID = request.form['edit_option']
+    ID_string = str(ID)
+    datas_to_fill = []
+    for element in table:
+        if element[0] == ID_string:
+            datas_to_fill.append(element)
+    return render_template("form.html",  story_id=story_id, form_data=datas_to_fill)
 
 @app.route('/story', methods=['GET', 'POST'])
 def story_create(story_id=None):
@@ -52,11 +58,11 @@ def add_data():
     data_list.append(request.form['status'])
     table.append(data_list)
     write_table_to_file(table)
-    return home_list()
+    return render_template("list.html", data_list=table)
 
 
 @app.route('/delete-story', methods=['POST'])
-def delete_data():
+def delete_data(data_list=[]):
     table = get_table_from_file()
     ID = request.form['delete']
     ID_string = str(ID)
@@ -64,29 +70,29 @@ def delete_data():
         if element[0] == ID_string:
             table.remove(element)
     write_table_to_file(table)
-    return home_list()
+    return render_template("list.html", data_list=table)
 
 
-@app.route('/story/<story_id>', methods=['GET', 'POST'])
-def edit_story(story_id=None, data_list=[]):
+@app.route("/edit-story", methods=['GET', 'POST'])
+def edit_story(data_list=[]):
     table = get_table_from_file()
-    ID = request.form['edit']
+    ID = request.form["edit"]
     ID_string = str(ID)
-    update_list = [element for element in table if element[0] == ID_string]
-    flatten_list = [item for sublist in update_list for item in sublist]
-    story_id = ID_string
     changed_story = []
-    """
-    data_list.append(request.form['story-title'])
-    data_list.append(request.form['user-story'])
-    data_list.append(request.form['accept-crit'])
-    data_list.append(request.form['bussines-value'])
-    data_list.append(request.form['estimation'])
-    data_list.append(request.form['status'])
-    """
-    #majd megkeresi a módosított lista ID-t a table-ben
-    #ha megvan akkor az egész sort KICSERÉLI
-    return render_template("form.html", story_id=story_id, data_list=flatten_list)
+    changed_story.append(ID_string)
+    changed_story.append(request.form['changed-story-title'])
+    changed_story.append(request.form['changed-user-story'])
+    changed_story.append(request.form['changed-accept-crit'])
+    changed_story.append(request.form['changed-bussines-value'])
+    changed_story.append(request.form['changed-estimation'])
+    changed_story.append(request.form['changed-status'])
+    for element in table:
+        for index, item in enumerate(element):
+            if item == ID_string:
+                table.remove(element)
+                table.insert(index, changed_story)
+    write_table_to_file(table)
+    return render_template("list.html", data_list=table)
 
 
 if __name__ == '__main__':
