a9b525db58edd055344e266494984b56408f9848
maylandertamas <maylandertamas@gmail.com>
2017-04-27 11:10:59 +0200

    Fixed ID generator and table write

--- common.py
+++ common.py
@@ -8,16 +8,14 @@ def get_table_from_file(file_name="stories.csv"):
 def write_table_to_file(table, file_name="stories.csv"):
     with open(file_name, "w") as file:
         for record in table:
-            row = ';'.join(record)
+            stripped_record = [element.replace('\r\n', ' ') for element in record]
+            row = ';'.join(stripped_record)
             file.write(row + "\n")
 
 
-def ID_generator():
-    table = get_table_from_file()
-    max_value = ""
-    for item in range(len(table)):
-        for element in table:
-            max_value = max(element)
-    print(max_value)
-    max_index = my_list.index(max_value)
-    return None
+def ID_generator(table):
+    IDs_in_table = [int(id_number[0]) for id_number in table]
+    if not IDs_in_table:
+        return str(1)
+    else:
+        return str(max(IDs_in_table) + 1)
